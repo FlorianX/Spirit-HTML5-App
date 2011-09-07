7c3f8246c34b747d778e072f5a5a0abc1ab3f368
Florian Schuhmann <schuhmann.florian@googlemail.com>
2011-09-07 17:26:30 +0200

    added GoogleAnalytics to track the usage

--- src/org/unsane/spiritHtml5App/SpiritHtml5AppActivity.java
+++ src/org/unsane/spiritHtml5App/SpiritHtml5AppActivity.java
@@ -1,5 +1,7 @@
 package org.unsane.spiritHtml5App;
 
+import com.google.android.apps.analytics.GoogleAnalyticsTracker;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
@@ -7,10 +9,19 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class SpiritHtml5AppActivity extends Activity {
+	
+	GoogleAnalyticsTracker tracker;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        tracker = GoogleAnalyticsTracker.getInstance();
+
+        // Start the tracker in manual dispatch mode...
+        tracker.startNewSession("UA-20869150-2", 5, this);
+        
         setContentView(R.layout.main);
         /**
          * @see http://www.youtube.com/watch?v=uVqp1zcMfbE
@@ -25,8 +36,22 @@ public class SpiritHtml5AppActivity extends Activity {
 
         // use the WebChromeClient
         webView.setWebChromeClient(new WebChromeClient());
-        
+
         // load the html files
         webView.loadUrl("file:///android_asset/www/index.html");
+        tracker.trackPageView("/SpiritHtml5App");
     }
+    
+    @Override
+    protected void onDestroy() {
+      super.onDestroy();
+      // Stop the tracker when it is no longer needed.
+      tracker.stopSession();
+    }
+    
+    @Override
+    public void onResume() {
+  	  tracker.trackPageView("/SpiritHtml5AppResumed");
+  	  super.onResume();
+  	}
 }
